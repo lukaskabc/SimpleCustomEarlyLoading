2656a1eb22b133d995b8afceb7830bd4c7263324
Lukáš Kaňka <lukaskabc@gmail.com>
2025-03-29 22:08:20 +0100

    use only single GL texture unit

---
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/elements/ApngTextureElement.java
@@ -0,0 +1,12 @@
+package cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.elements;
+
+import cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.reflection.CSB;
+
+public class ApngTextureElement implements ElementSupplier {
+
+
+    @Override
+    public void render(CSB csb, int frame) {
+
+    }
+}
