83043b543ee96e3b68a7457f6a8c298ea5ca7c33
Lukáš Kaňka <lukaskabc@gmail.com>
2025-04-11 22:50:02 +0200

    introduce element position and size configuration

--- src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/Element.java
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/Element.java
@@ -2,11 +2,18 @@ package cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.co
 
 public class Element {
     private String image = null;
-    private ElementType type = ElementType.ABSOLUTE;
-    private float x1;
-    private float x2;
-    private float y1;
-    private float y2;
+    private ElementPosition position;
+
+    public String getExtension() {
+        if (image == null) {
+            throw new ConfigurationException("No image specified");
+        }
+        int index = image.lastIndexOf('.');
+        if (index == -1) {
+            throw new ConfigurationException("No extension specified for file " + image);
+        }
+        return image.substring(index);
+    }
 
     public String getImage() {
         return image;
@@ -16,47 +23,11 @@ public class Element {
         this.image = image;
     }
 
-    public ElementType getType() {
-        return type;
+    public ElementPosition getPosition() {
+        return position;
     }
 
-    public void setType(ElementType type) {
-        this.type = type;
-    }
-
-    public float getX1() {
-        return x1;
-    }
-
-    public void setX1(float x1) {
-        this.x1 = x1;
-    }
-
-    public float getX2() {
-        return x2;
-    }
-
-    public void setX2(float x2) {
-        this.x2 = x2;
-    }
-
-    public float getY1() {
-        return y1;
-    }
-
-    public void setY1(float y1) {
-        this.y1 = y1;
-    }
-
-    public float getY2() {
-        return y2;
-    }
-
-    public void setY2(float y2) {
-        this.y2 = y2;
-    }
-
-    public float[] getCoords() {
-        return new float[]{x1, x2, y1, y2};
+    public void setPosition(ElementPosition position) {
+        this.position = position;
     }
 }
