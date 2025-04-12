8461b7faa448f094f42c49cb34be4b12c79b9bee
Lukáš Kaňka <lukaskabc@gmail.com>
2025-04-12 17:18:03 +0200

    add javadoc for ElementPosition and introduce element anchor

--- src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/ElementPosition.java
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/ElementPosition.java
@@ -4,35 +4,71 @@ package cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.co
  * Represents the element position and size on the screen.
  */
 public class ElementPosition {
-    private Unit positionType = Unit.PIXELS;
-    private Unit sizeType = Unit.PIXELS;
+    /**
+     * Denotes the alignment of the element to the position defined by {@link #x} and {@link #y}.
+     */
+    private Anchor positionAnchor = Anchor.CENTER;
+    /**
+     * Unit for {@link #x} and {@link #y} values.
+     */
+    private Unit positionUnit = Unit.PIXELS;
+    /**
+     * Unit for {@link #width} and {@link #height} values.
+     */
+    private Unit sizeUnit = Unit.PIXELS;
+    /**
+     * X position of the element anchor on the screen.
+     */
     private float x = 0;
+    /**
+     * Y position of the element anchor on the screen.
+     */
     private float y = 0;
+    /**
+     * Width of the element on the screen.
+     * If not set by configuration, {@link Float#NaN} is used.
+     * In that case {@link #height} should be set while width is calculated to keep the aspect ratio.
+     */
     private float width = Float.NaN;
+    /**
+     * Height of the element on the screen.
+     * If not set by configuration, {@link Float#NaN} is used.
+     * In that case {@link #width} should be set while height is calculated to keep the aspect ratio.
+     */
     private float height = Float.NaN;
 
+    /**
+     * Checks whether the element has a width set.
+     *
+     * @return true when the width is finite (not NaN or infinite), false otherwise
+     */
     public boolean hasWidth() {
         return Float.isFinite(width);
     }
 
+    /**
+     * Checks whether the element has a height set.
+     *
+     * @return true when the height is finite (not NaN or infinite), false otherwise
+     */
     public boolean hasHeight() {
         return Float.isFinite(height);
     }
 
-    public Unit getPositionType() {
-        return positionType;
+    public Unit getPositionUnit() {
+        return positionUnit;
     }
 
-    public void setPositionType(Unit positionType) {
-        this.positionType = positionType;
+    public void setPositionUnit(Unit positionUnit) {
+        this.positionUnit = positionUnit;
     }
 
-    public Unit getSizeType() {
-        return sizeType;
+    public Unit getSizeUnit() {
+        return sizeUnit;
     }
 
-    public void setSizeType(Unit sizeType) {
-        this.sizeType = sizeType;
+    public void setSizeUnit(Unit sizeUnit) {
+        this.sizeUnit = sizeUnit;
     }
 
     public float getX() {
@@ -67,24 +103,126 @@ public class ElementPosition {
         this.height = height;
     }
 
+    public Anchor getPositionAnchor() {
+        return positionAnchor;
+    }
+
+    public void setPositionAnchor(Anchor positionAnchor) {
+        this.positionAnchor = positionAnchor;
+    }
+
     public enum Unit {
+        /**
+         * Absolute size in pixels on the screen.
+         */
         PIXELS,
+        /**
+         * Percentage of the window size.
+         * <p>
+         * Allowed values: {@code 0 - 100}
+         */
         PERCENTAGE,
     }
+
+    public enum Anchor {
+        /**
+         * Anchor is in the top left corner of the screen.
+         * <pre><code>
+         * +-------------+
+         * |#            |
+         * |             |
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        TOP_LEFT,
+        /**
+         * Anchor is in the top right corner of the screen.
+         * <pre><code>
+         * +-------------+
+         * |            #|
+         * |             |
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        TOP_RIGHT,
+        /**
+         * Anchor is in the bottom left corner of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |             |
+         * |#            |
+         * +-------------+
+         * </code></pre>
+         */
+        BOTTOM_LEFT,
+        /**
+         * Anchor is in the bottom right corner of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |             |
+         * |            #|
+         * +-------------+
+         * </code></pre>
+         */
+        BOTTOM_RIGHT,
+        /**
+         * Anchor is in the center of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |      #      |
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        CENTER,
+        /**
+         * Anchor is in the top center of the screen.
+         * <pre><code>
+         * +-------------+
+         * |      #      |
+         * |             |
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        TOP_CENTER,
+        /**
+         * Anchor is in the bottom center of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |             |
+         * |      #      |
+         * +-------------+
+         * </code></pre>
+         */
+        BOTTOM_CENTER,
+        /**
+         * Anchor is in the left center of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |#            |
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        LEFT_CENTER,
+        /**
+         * Anchor is in the right center of the screen.
+         * <pre><code>
+         * +-------------+
+         * |             |
+         * |            #|
+         * |             |
+         * +-------------+
+         * </code></pre>
+         */
+        RIGHT_CENTER,
+    }
 }
-/*
-Position
-absolute - pixels
-percentage on the screen
-
-Size
-absolute - pixels
-percentage on the screen
-
-
-width + height
-or just one with keeping aspect ratio
-
-
-
- */
