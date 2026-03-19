7ca1508800d1a915485477230c0214463bcd994b
Lukáš Kaňka <lukaskabc@gmail.com>
2026-03-19 21:09:59 +0100

    Add documentation for new display conditions and loading textures

--- src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/condition/LoadingPercentageCondition.java
+++ src/main/java/cz/lukaskabc/minecraft/mod_loader/loading/simple_custom_early_loading/config/condition/LoadingPercentageCondition.java
@@ -3,14 +3,11 @@ package cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.co
 import cz.lukaskabc.minecraft.mod_loader.loading.simple_custom_early_loading.helper.LoadingProgressHelper;
 import net.neoforged.fml.loading.progress.ProgressMeter;
 import net.neoforged.fml.loading.progress.StartupNotificationManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class LoadingPercentageCondition extends DisplayCondition {
-    private static final Logger LOG = LogManager.getLogger();
     private boolean minimumInclusive = true;
     private int minimum;
 
@@ -38,8 +35,6 @@ public class LoadingPercentageCondition extends DisplayCondition {
     @Override
     public boolean shouldDisplay() {
         List<ProgressMeter> currentProgress = StartupNotificationManager.getCurrentProgress();
-        currentProgress.stream().map(ProgressMeter::name).forEach(label -> LOG.info("Current loading label: {}", label));
-        LOG.info("=============");
         final double mcProgress = (int) (LoadingProgressHelper.getMinecraftProgress(currentProgress) * 100f);
         return minHolds(mcProgress) && maxHolds(mcProgress);
     }
